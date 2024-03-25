55883d82c3ad834d8adf2fd83cab8d58111f5875
Sajal <sajal.moni@gmail.com>
2024-03-25 16:03:41 +0200

    CreateBlog component form add cancel button

--- bloglist-frontend/src/components/CreateBlog.jsx
+++ bloglist-frontend/src/components/CreateBlog.jsx
@@ -27,37 +27,39 @@ const CreateBlog = ({ handleBlogSubmit }) => {
       {!showForm ? (
         <button onClick={handleNewBlogClick}>New Blog</button>
       ) : (
-        <form onSubmit={handleFormSubmit}>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="author">Author:</label>
-            <input
-              type="text"
-              id="author"
-              value={author}
-              onChange={(e) => setAuthor(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="url">URL:</label>
-            <input
-              type="text"
-              id="url"
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-            />
-          </div>
-
-          <button type="submit">Create Blog</button>
-        </form>
+        <div>
+          <form onSubmit={handleFormSubmit}>
+            <div>
+              <label htmlFor="title">Title:</label>
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="author">Author:</label>
+              <input
+                type="text"
+                id="author"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="url">URL:</label>
+              <input
+                type="text"
+                id="url"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+              />
+            </div>
+            <button type="submit">Create Blog</button>
+          </form>
+          <button onClick={() => setShowForm(false)}>Cancel</button>
+        </div>
       )}
     </div>
   );
